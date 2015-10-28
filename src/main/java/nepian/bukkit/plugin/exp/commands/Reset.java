130fc6d3e2a64a2a89bf0f79101384f9acecaf90
Nepian <nepia.ms@gmail.com>
2015-10-29 03:57:01 +0900

    リセットコマンド時にプレイヤー指定を追加（テスト中）

--- src/main/java/nepian/bukkit/plugin/exp/commands/Reset.java
+++ src/main/java/nepian/bukkit/plugin/exp/commands/Reset.java
@@ -5,6 +5,7 @@ import nepian.bukkit.plugin.exp.Main;
 import nepian.bukkit.plugin.exp.configration.Lang;
 import nepian.bukkit.plugin.exp.configration.Permissions;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -14,9 +15,34 @@ public class Reset {
 	public static void useCommand(CommandSender sender, String label, String[] args) {
 		if (!Permissions.RESET.hasPermission(sender, label, args)) return;
 
-		Player player = (Player)sender;
-		ExpManager.resetExp(player);
-		String msg = Lang.EXP_RESET.get();
-		plugin.sendMessage(sender, msg);
+		if (args.length != 2) {
+			plugin.sendMessage(sender, Lang.ERROR_COMMAND.get());
+			plugin.sendMessage(sender, "Žg‚¢•û: /exp reset <player>");
+			return;
+		}
+
+		Player target = getPlayer(args[1]);
+
+		if (target == null) {
+			plugin.sendMessage(sender, Lang.ERROR_NOT_ONLINE_PLAYER.get());
+			return;
+		}
+
+		ExpManager.resetExp(target);
+
+		plugin.sendMessage(sender, Lang.EXP_RESET_SENDER_MESSAGE.get()
+				.replace("{target}", target.getName()));
+		plugin.sendMessage(target, Lang.EXP_RESET_TARGET_MESSAGE.get()
+				.replace("{sender}", sender.getName()));
+	}
+
+	private static Player getPlayer(String name) {
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (player.getName().equalsIgnoreCase(name)) {
+				return player;
+			}
+		}
+
+		return null;
 	}
 }
